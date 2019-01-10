11484c29d07e30a3e6aeeca28c4d8d0c9384b68c
GeneKao <kao.gene@gmail.com>
2019-01-10 10:25:44 +0800

    add simple example for day 1 class

--- src/day_1/final/02_python102simple.py
+++ src/day_1/final/02_python102simple.py
@@ -11,39 +11,9 @@ class Point(object):
     """
 
     def __init__(self, x=0.0, y=0.0, z=0.0):
-        self._x = 0.0
-        self._y = 0.0
-        self._z = 0.0
         self.x = x
         self.y = y
         self.z = z
-
-    @property
-    def x(self):
-        """float: The X coordinate of the point."""
-        return self._x
-
-    @x.setter
-    def x(self, x):
-        self._x = float(x)
-
-    @property
-    def y(self):
-        """float: The Y coordinate of the point."""
-        return self._y
-
-    @y.setter
-    def y(self, y):
-        self._y = float(y)
-
-    @property
-    def z(self):
-        """float: The Z coordinate of the point."""
-        return self._z
-
-    @z.setter
-    def z(self, z):
-        self._z = float(z)
 
     def __repr__(self):
         return 'Point({0}, {1}, {2})'.format(self.x, self.y, self.z)
@@ -61,30 +31,9 @@ class Line(object):
     """
 
     def __init__(self, p1, p2):
-        self._start = None
-        self._end = None
         self.start = p1
         self.end = p2
 
-    @property
-    def start(self):
-        """Point: the start point."""
-        return self._start
-
-    @start.setter
-    def start(self, point):
-        self._start = Point(*point)
-
-    @property
-    def end(self):
-        """Point: the end point."""
-        return self._end
-
-    @end.setter
-    def end(self, point):
-        self._end = Point(*point)
-
-    @property
     def midpoint(self):
         """Point: The midpoint between start and end."""
         return Point(0.5 * (self.start.x + self.end.x),
